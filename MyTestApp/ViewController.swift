f110831bd4b0e429565676d2b4f42479d5a76217
Joe Kletz <joekletz@gmail.com>
2020-10-06 10:45:09 +0100

    Initial Commit

---
+++ MyTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTestApp
+//
+//  Created by Joe Kletz on 06/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
