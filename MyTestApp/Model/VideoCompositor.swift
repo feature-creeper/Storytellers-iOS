3578e38ef1f0f5a55076fe6ad8310f791a959046
Joe Kletz <joekletz@gmail.com>
2020-10-28 17:11:58 +0000

    Updated output video look

--- MyTestApp/Model/VideoCompositor.swift
+++ MyTestApp/Model/VideoCompositor.swift
@@ -12,6 +12,10 @@ import AVFoundation
 import Photos
 import CoreData
 
+//PROBABLY COULD MAKE COMPOSITION EASIER IF APPLIED TO PARENT
+//        textLayer.isGeometryFlipped = true
+
+
 class VideoCompositor {
     
     var pageTimes : [(Int, Int)]
@@ -20,16 +24,10 @@ class VideoCompositor {
     
     let view:UIView?
     
-    //let storyText:StoryText
     let text:[[String]]
     
     let bookID:String
     
-//    init(_ view: UIView, pageTimes: [(Int, Int)],storyText:StoryText) {
-//        self.view = view
-//        self.pageTimes = pageTimes
-//        self.storyText = storyText
-//    }
     init(_ view: UIView, pageTimes: [(Int, Int)],storyText:[[String]], bookID : String) {
         self.view = view
         self.pageTimes = pageTimes
@@ -46,12 +44,7 @@ class VideoCompositor {
         // get video track
         let vtrack =  vidAsset.tracks(withMediaType: AVMediaType.video)
         
-        
-        
-        
         let assetAudioTrack: AVAssetTrack = vidAsset.tracks(withMediaType: AVMediaType.audio)[0]
-        
-        
         
         
         let videoTrack: AVAssetTrack = vtrack[0]
@@ -69,9 +62,6 @@ class VideoCompositor {
             do {
                 try compositionvideoTrack.insertTimeRange(vid_timerange, of: videoTrack, at: CMTime.zero)
                 
-                
-                //                let a = composition.addMutableTrack(withMediaType: AVMediaType.audio, preferredTrackID: kCMPersistentTrackID_Invalid)
-                //                a?.insertTimeRange(vid_timerange, of: assetAudioTrack, at: CMTime.zero)
             } catch {
                 print("error")
             }
@@ -88,14 +78,9 @@ class VideoCompositor {
             do {
                 try compositionaudioTrack.insertTimeRange(vid_timerange, of: assetAudioTrack, at: CMTime.zero)
                 
-                
-                //                let a = composition.addMutableTrack(withMediaType: AVMediaType.audio, preferredTrackID: kCMPersistentTrackID_Invalid)
-                //                a?.insertTimeRange(vid_timerange, of: assetAudioTrack, at: CMTime.zero)
             } catch {
                 print("error")
             }
-            
-//            compositionvideoTrack.preferredTransform = videoTrack.preferredTransform
             
         } else {
             print("unable to add video track")
@@ -127,12 +112,6 @@ class VideoCompositor {
         layercomposition.animationTool = AVVideoCompositionCoreAnimationTool(postProcessingAsVideoLayer: videolayer, in: parentlayer)
         
         
-        
-        
-        
-        /* Add audio track */
-        //        AVMutableCompositionTrack *compositionAudioTrack = [composition addMutableTrackWithMediaType:AVMediaTypeAudio preferredTrackID:kCMPersistentTrackID_Invalid];
-        //        [compositionAudioTrack insertTimeRange:fullTime ofTrack:sourceAudioTrack atTime:kCMTimeZero error:nil];
         
         
         // instruction for watermark
@@ -179,20 +158,6 @@ class VideoCompositor {
                 
                 self.myurl = movieDestinationUrl as URL
                 
-//                let fetch = NSFetchRequest<Book>(entityName: "Book")
-//                fetch.predicate = NSPredicate(format: "id == %@", bookID)
-//                
-//                let appDelegate = UIApplication.shared.delegate as! AppDelegate
-//                let context = appDelegate.persistentContainer.viewContext
-//                
-//                let book : Book = try! context.fetch(fetch).first! as Book
-//                
-//                
-//                let video = VideoMO(context: context)
-//                video.filename = videoFileName
-//                
-//                book.addToVideos(video)
-                
                 DatabaseHelper.shared.createVideoMO(bookID: bookID, videoFileName: videoFileName, videoURL: self.myurl!) {
                     print("SAVING COMPLETE")
                     
@@ -200,117 +165,87 @@ class VideoCompositor {
                 }
                 
                 
-                
-                
-//                PHPhotoLibrary.shared().performChanges({
-//                    PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: movieDestinationUrl as URL)
-//
-//                }) { saved, error in
-//                    if saved {
-//                        print("Saved")
-//                    }
-//                }
-                
-//                self.playVideo()
-            
             }
         })
     }
     
-//    func playVideo() {
-//        let player = AVPlayer(url: myurl!)
-//        let playerLayer = AVPlayerLayer(player: player)
-//        playerLayer.frame = self.view!.bounds
-//        self.view!.layer.addSublayer(playerLayer)
-//        player.play()
-//        print("playing...")
-//    }
-    
-    
-    //U can add another white layer that fades in a second before
-    
     func createTextLayers(parentLayer:CALayer, pageHeight : CGFloat, size : CGSize) {
         
         
-        for page in pageTimes {
-            
-            print("PAGE ------------>")
-
-            print(text[0][page.0])
-            
-            let animationParentLayer = CALayer()
-            animationParentLayer.opacity = 0
-            animationParentLayer.backgroundColor = UIColor.white.cgColor
-            animationParentLayer.frame = CGRect(x: 0, y: 0, width: size.width, height: pageHeight)
-            
-            let titleLayer = CATextLayer()
-            
-            titleLayer.backgroundColor = UIColor.white.cgColor
-//            titleLayer.string = storyText.story[page.0]//"STORY PAGE \(page.0)"//
-            titleLayer.string = text[0][page.0]
-            titleLayer.font = UIFont(name: "Helvetica", size: 56)
-            titleLayer.foregroundColor = UIColor.black.cgColor
-            titleLayer.shadowOpacity = 0.5
-            titleLayer.alignmentMode = CATextLayerAlignmentMode.left
-            titleLayer.frame = CGRect(x: 0, y: 0, width: size.width, height: pageHeight)
-            titleLayer.isWrapped = true
-            titleLayer.fontSize = 70
-            titleLayer.shadowOpacity = 0
-            titleLayer.bounds.size.width = size.width - 70
-            titleLayer.bounds.size.height = pageHeight - 70
-            
-            animationParentLayer.addSublayer(titleLayer)
-            parentLayer.addSublayer(animationParentLayer)
-            
-            
-            //Fade in animation
-            
-            var startTime : Double = 0.1;
-            for item in pageTimes {
-                if item.0 < page.0 {
-                    startTime += Double(item.1)
-                }
-            }
-            
-            let startTimeMilliseconds = startTime / 1000
-            
-            
-            
-            let animation = CABasicAnimation(keyPath: "opacity")
-            animation.fromValue = 0
-            animation.toValue = 1
-            animation.duration = 1
-            animation.beginTime = CFTimeInterval(floatLiteral: startTimeMilliseconds)//CFTimeInterval(exactly: 5)!//AVCoreAnimationBeginTimeAtZero//
-            animation.fillMode = CAMediaTimingFillMode.forwards;
-            animation.isRemovedOnCompletion = false
-            animationParentLayer.add(animation, forKey: "opacity")
-            
-            
-            //Fade out animation
-            
-            var outTime : Double = 0;
-            for item in pageTimes {
-                if item.0 <= page.0 {
-                    outTime += Double(item.1)
-                }
-            }
-            
-            let outTimeMilliseconds = outTime / 1000 - 1
-            
-            let fadeOutAnimation = CABasicAnimation(keyPath: "opacity")
-            fadeOutAnimation.fromValue = 1
-            fadeOutAnimation.toValue = 0
-            fadeOutAnimation.duration = 1
-            fadeOutAnimation.beginTime = CFTimeInterval(floatLiteral: outTimeMilliseconds)//CFTimeInterval(exactly: 5)!//AVCoreAnimationBeginTimeAtZero//
-            fadeOutAnimation.fillMode = CAMediaTimingFillMode.forwards;
-            fadeOutAnimation.isRemovedOnCompletion = false
-            
-            titleLayer.add(fadeOutAnimation, forKey: "opacity")
-            
-            
-            print("PAGE \(page.0) IN \(startTimeMilliseconds) OUT \(outTimeMilliseconds)")
-        }
+        //for page in pageTimes {
         
+        let pageBackgroundLayer = CALayer()
+        pageBackgroundLayer.backgroundColor = UIColor.white.cgColor
+        pageBackgroundLayer.frame = CGRect(x: 0, y: 0, width: size.width * 2, height: pageHeight * 2)
+        
+//        pageBackgroundLayer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+//        pageBackgroundLayer.anchorPoint = CGPoint(x: 0, y: 0)
+        pageBackgroundLayer.position = CGPoint(x: 0, y: 0)
+        
+        pageBackgroundLayer.shadowColor = UIColor.black.cgColor
+        pageBackgroundLayer.shadowOffset = CGSize(width: 0, height: 0)
+        pageBackgroundLayer.shadowOpacity = 0.4
+        pageBackgroundLayer.shadowRadius = 15.0
+        
+        let textBorder : CGFloat = 70
+        
+        let textLayer = CATextLayer()
+        textLayer.backgroundColor = UIColor.white.cgColor
+        textLayer.string = "TEST PAGE ASDFAF ASDG ASDASD S GSGS GGSD GG G SGTEST PAGE ASDFAF ASDG ASDASD S GSGS GGSD GG G SGTEST PAGE ASDFAF ASDG ASDASD S GSGS GGSD GG G SGTEST PAGE ASDFAF ASDG ASDASD"//text[0][page.0]
+        textLayer.font = UIFont(name: Globals.easyRead, size: 0)
+        textLayer.fontSize = 45
+        textLayer.foregroundColor = UIColor.black.cgColor
+        textLayer.alignmentMode = CATextLayerAlignmentMode.left
+        
+        textLayer.anchorPoint = CGPoint(x: 0, y: 0)
+        //IGNORE FRAME X, Y
+        //textLayer.frame = CGRect(x: 0, y: 0, width: size.width, height: pageHeight)
+        textLayer.position = CGPoint(x: size.width + textBorder, y: pageHeight + textBorder)
+        textLayer.isWrapped = true
+        textLayer.bounds.size.width = size.width - (textBorder * 2)
+        textLayer.bounds.size.height = pageHeight - (textBorder * 2)
+    
+
+        
+        pageBackgroundLayer.addSublayer(textLayer)
+        parentLayer.addSublayer(pageBackgroundLayer)
+        
+        //CREATE TIME FROM BEGINNING OF SESSION
+        
+        //            var outTime : Double = 0;
+        //            for item in pageTimes {
+        //                if item.0 <= page.0 {
+        //                    outTime += Double(item.1)
+        //                }
+        //            }
+        //            let startTimeMilliseconds = outTime / 1000
+        
+        print("STARTTIME")
+        //print(startTimeMilliseconds)
+        
+        
+        let groupAnimation = CAAnimationGroup()
+        groupAnimation.beginTime = 1
+        groupAnimation.duration = 0.7
+        
+//        let animation = CABasicAnimation(keyPath: "position")
+//        animation.fromValue = pageBackgroundLayer.position
+//        animation.toValue = [-(pageBackgroundLayer.bounds.width * 1.5) ,  pageBackgroundLayer.position.y]
+//        animation.duration = 1
+//        animation.beginTime = 1
+        //animation.fillMode = CAMediaTimingFillMode.forwards;
+        
+        let position = CABasicAnimation(keyPath: "position")
+        position.fromValue = pageBackgroundLayer.position
+        position.toValue = [-(pageBackgroundLayer.bounds.width * 1.5) ,  pageBackgroundLayer.position.y]
+        let rotate = CABasicAnimation(keyPath: "transform.rotation")
+        rotate.fromValue = 0.0
+        rotate.toValue = .pi/180.0 * 20
+        
+        //animation.isRemovedOnCompletion = false
+        groupAnimation.animations = [position,rotate]
+        pageBackgroundLayer.add(groupAnimation, forKey: nil)
+        //}
     }
 }
 
@@ -326,3 +261,89 @@ extension FileManager {
         }
     }
 }
+
+
+/*
+ //OLD FADE ANIMATED PAGES
+ func createTextLayers(parentLayer:CALayer, pageHeight : CGFloat, size : CGSize) {
+ 
+ 
+ for page in pageTimes {
+ 
+ print("PAGE ------------>")
+ 
+ print(text[0][page.0])
+ 
+ let animationParentLayer = CALayer()
+ animationParentLayer.opacity = 0
+ animationParentLayer.backgroundColor = UIColor.white.cgColor
+ animationParentLayer.frame = CGRect(x: 0, y: 0, width: size.width, height: pageHeight)
+ 
+ let titleLayer = CATextLayer()
+ 
+ titleLayer.backgroundColor = UIColor.white.cgColor
+ 
+ titleLayer.string = text[0][page.0]
+ titleLayer.font = UIFont(name: "Helvetica", size: 56)
+ titleLayer.foregroundColor = UIColor.black.cgColor
+ titleLayer.shadowOpacity = 0.5
+ titleLayer.alignmentMode = CATextLayerAlignmentMode.left
+ titleLayer.frame = CGRect(x: 0, y: 0, width: size.width, height: pageHeight)
+ titleLayer.isWrapped = true
+ titleLayer.fontSize = 70
+ titleLayer.shadowOpacity = 0
+ titleLayer.bounds.size.width = size.width - 70
+ titleLayer.bounds.size.height = pageHeight - 70
+ 
+ animationParentLayer.addSublayer(titleLayer)
+ parentLayer.addSublayer(animationParentLayer)
+ 
+ 
+ //Fade in animation
+ 
+ var startTime : Double = 0.1;
+ for item in pageTimes {
+ if item.0 < page.0 {
+ startTime += Double(item.1)
+ }
+ }
+ 
+ let startTimeMilliseconds = startTime / 1000
+ 
+ 
+ 
+ let animation = CABasicAnimation(keyPath: "opacity")
+ animation.fromValue = 0
+ animation.toValue = 1
+ animation.duration = 1
+ animation.beginTime = CFTimeInterval(floatLiteral: startTimeMilliseconds)//CFTimeInterval(exactly: 5)!//AVCoreAnimationBeginTimeAtZero//
+ animation.fillMode = CAMediaTimingFillMode.forwards;
+ animation.isRemovedOnCompletion = false
+ animationParentLayer.add(animation, forKey: "opacity")
+ 
+ 
+ //Fade out animation
+ 
+ var outTime : Double = 0;
+ for item in pageTimes {
+ if item.0 <= page.0 {
+ outTime += Double(item.1)
+ }
+ }
+ 
+ let outTimeMilliseconds = outTime / 1000 - 1
+ 
+ let fadeOutAnimation = CABasicAnimation(keyPath: "opacity")
+ fadeOutAnimation.fromValue = 1
+ fadeOutAnimation.toValue = 0
+ fadeOutAnimation.duration = 1
+ fadeOutAnimation.beginTime = CFTimeInterval(floatLiteral: outTimeMilliseconds)//CFTimeInterval(exactly: 5)!//AVCoreAnimationBeginTimeAtZero//
+ fadeOutAnimation.fillMode = CAMediaTimingFillMode.forwards;
+ fadeOutAnimation.isRemovedOnCompletion = false
+ 
+ titleLayer.add(fadeOutAnimation, forKey: "opacity")
+ 
+ 
+ print("PAGE \(page.0) IN \(startTimeMilliseconds) OUT \(outTimeMilliseconds)")
+ }
+ */
